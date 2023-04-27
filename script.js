86d624db39ef641fcc7dfc6f3e8b795979c31843
Serhii Parkhomenko <parsvlad@gmail.com>
2023-04-27 20:11:36 +0300

    Initial

---
+++ script.js
@@ -0,0 +1,26 @@
+const ratingPoints = document.querySelectorAll('.rating__score__point');
+const submitButton = document.querySelector('.rating__submit');
+
+ratingPoints.forEach(point => {
+  point.addEventListener('click', () => {
+    removeActive();
+    point.classList.toggle('active');
+  });
+});
+
+function removeActive() {
+  ratingPoints.forEach(point => {
+    if (point.classList.contains('active')) {
+      point.classList.remove('active');
+    }
+  });
+}
+
+submitButton.addEventListener('click', () => {
+  ratingPoints.forEach(point => {
+    if (point.classList.contains('active')) {
+      document.querySelector('.raiting__point').innerHTML = point.innerText;
+    }
+  });
+  document.querySelector('.popup').style.display = 'grid';
+});
